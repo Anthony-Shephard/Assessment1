ca6c6551488fca80f9d9d24c385c8400f44d03e3
Anthony-Shephard <anthonygs@live.com.au>
2015-05-11 20:18:42 +1000

    sprite.js error

--- player.js
+++ player.js
@@ -1,16 +1,41 @@
+var LEFT = 0;
+var RIGHT = 1;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_SHOOT_LEFT = 3;
+var ANIM_CLIMB = 4;
+var ANIM_IDLE_RIGHT = 5;
+var ANIM_JUMP_RIGHT = 6;
+var ANIM_WALK_RIGHT = 7;
+var ANIM_SHOOT_RIGHT = 8;
+var ANIM_MAX = 9;
+
 var Player = function() 
 	{
-		this.image = document.createElement("img");
+		this.sprite = new Sprite("ChuckNorris.png");
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[0, 1, 2, 3, 4, 5, 6, 7]);
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[8, 9, 10, 11, 12]);
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[52, 53, 54, 55, 56, 57, 58, 59]);
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[60, 61, 62, 63, 64]);
+		this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+			[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+
+		for(var i=0; i<ANIM_MAX; i++)
+		{
+			this.sprite.setAnimationOffset(i, -55, -87)
+		}
+
 		this.position = new Vector2 ();
-		this.position.Set(9*35, 0*35);
+		this.position.Set(9*35, 9*35);
 		this.width = 159;
 		this.height = 163;
-		this.image.src = "hero.png";
-		this.velocityX = 0;
-		this.velocityXY = 0;
-		this.angularVelocity = 0;
-		this.rotation = 0;
-		
 		this.velocity = new Vector2 ();
 		this.falling = true;
 		this.jumping = false;
@@ -19,7 +44,8 @@ var Player = function()
 
 Player.prototype.update = function(deltaTime)
 	{
-		// we’ll insert code here later
+		this.sprite.update;
+
 		 var left = false;
 		 var right = false;
 		 var jump = false;
@@ -141,6 +167,7 @@ Player.prototype.update = function(deltaTime)
 
 Player.prototype.draw = function()
 	{
+		this.sprite.draw(context, this.position.x, this.position.y);
 		context.save();
 		context.translate(this.x, this.y);
 		context.rotate(this.rotation);
